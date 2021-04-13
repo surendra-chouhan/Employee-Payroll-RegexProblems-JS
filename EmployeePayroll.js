88462bfb41366ef9bd07a1adc115a34b52f8192d
surendra-chouhan <chouhansurendra028@gmail.com>
2021-04-13 20:46:26 +0530

    Added logic to extend Employee Payroll to store gender and startdate

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,11 +1,15 @@
 class EmployeePayroll {
     id;
     salary;
+    gender;
+    startdate;
 
-    constructor(id, name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params){
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startdate = params[4];
     }
 
     get name() {
@@ -17,7 +21,8 @@ class EmployeePayroll {
     }
 
     toString() {
-        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary;
+        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary +
+                ", gender = " + this.gender + ", startdate = " + this.startdate;
     }
 }
 
@@ -26,3 +31,6 @@ console.log(employeePayroll.toString());
 
 employeePayroll.name = "Omkar";
 console.log(employeePayroll.toString());
+
+let newemployeePayroll = new EmployeePayroll(2, "Rohit", 20000, "M", new Date("03-02-2019"));
+console.log(newemployeePayroll.toString());
